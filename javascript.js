4fe926be2b4026887c1abef50a33850abd3ff301
ldominguezlopez <134539227+ldominguezlopez@users.noreply.github.com>
2023-08-24 10:42:30 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,13 @@
+//Ejercicio 1
+var nombre = "Leslie"
+var apellido= "Dominguez"
+console.log(nombre + " " + apellido);
+
+//Ejercicio 2
+var num1 = 10
+var num2 = 5
+var suma = num1 + num2
+console.log(suma);
+var num3 = 20
+console.log(suma+num3);
+
